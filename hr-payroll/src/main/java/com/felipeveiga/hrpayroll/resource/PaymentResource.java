ef88a1413991c5a7d2fbb230e8a8193590c19b99
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-04-27 16:28:13 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/felipeveiga/hrpayroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/felipeveiga/hrpayroll/resource/PaymentResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.felipeveiga.hrpayroll.entities.Payment;
 import com.felipeveiga.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class PaymentResource {
@@ -15,11 +16,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/payments/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok().body(payment);
 	}
 	
-	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		Payment payment = new Payment("Alvim", 400.00, days);
+		return ResponseEntity.ok().body(payment);
+	}
 }
