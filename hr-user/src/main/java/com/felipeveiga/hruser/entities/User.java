f0c8d3a83bafdb490358ca0dbc1bd0d3bcde328f
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-05-02 13:33:31 -0300

    Database Seeding

--- hr-user/src/main/java/com/felipeveiga/hruser/entities/User.java
+++ hr-user/src/main/java/com/felipeveiga/hruser/entities/User.java
@@ -29,7 +29,7 @@ public class User implements Serializable{
 	private String password;
 	
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "USER_ROLE",
+	@JoinTable(name = "user_role",
 		joinColumns = @JoinColumn(name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id")
 			)
